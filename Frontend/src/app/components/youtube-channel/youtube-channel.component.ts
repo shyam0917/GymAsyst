8405a5381e3a0bfaf5f37bff987354b5a925b894
shyam <shyam@gmail.com>
2019-12-20 20:34:30 +0530

    added youtube player

--- Frontend/src/app/components/youtube-channel/youtube-channel.component.ts
+++ Frontend/src/app/components/youtube-channel/youtube-channel.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import { YoutubeService } from '../../services/youtube.service';
-
+import {DomSanitizer} from '@angular/platform-browser';
 import trainerChannel from '../../../assets/json/trainerChannel.json';
+import youTubeParser from 'youtube-parser';
 
 @Component({
   selector: 'app-youtube-channel',
@@ -13,15 +14,30 @@ import trainerChannel from '../../../assets/json/trainerChannel.json';
 export class YoutubeChannelComponent implements OnInit {
   videos: any[];
   paramData:any;
+  videourl:any;
 
   constructor(private youTubeService: YoutubeService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer) {
       this.route.params.subscribe( params => this.paramData=params );
      }
 
   ngOnInit() {
   this.getVideos();
   }
+
+
+  getVideoId(id){
+    this.videourl= this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" +id);
+    // youTubeParser.getURL('https://www.youtube.com/watch?v='+ id, {quality: 'medium', container: 'mp4'})
+    // .then(
+    //   function (urlList) {
+    //     // Access URLs.
+    //     console.log("hhh" + urlList[0]);
+    //   }
+    // );
+  }
+
 
   getVideos(){
     this.videos=trainerChannel.items;
